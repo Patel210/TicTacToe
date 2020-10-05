e5a1de5c59eee19194e13300a5dc639b5ffb83b1
Patel210 <pranavpatel2101997@gmail.com>
2020-10-05 21:38:05 +0530

    Added choose Character method to choose character for playing

--- src/com/training/tictactoegame/TicTacToeGame.java
+++ src/com/training/tictactoegame/TicTacToeGame.java
@@ -1,16 +1,37 @@
 package com.training.tictactoegame;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToeGame {
 	private static char[] board;
-	
+	private static final Scanner SC = new Scanner(System.in);
+	private static char USER, COMPUTER;
+
+	/**
+	 * Initializing board with empty spaces
+	 */
 	private static void createBoard() {
 		board = new char[10];
 		Arrays.fill(board, ' ');
 	}
-	
+
+	/**
+	 * To decide the character of the user
+	 */
+	private static void chooseCharacterForUser() {
+		System.out.println("Enter the Character you want to play for this game (X or O): ");
+		USER = SC.next().charAt(0);
+		if (USER == 'X' || USER == 'O')
+			COMPUTER = (USER == 'X') ? 'O' : 'X';
+		else {
+			System.out.println("Strictly enter (X or O)");
+			chooseCharacterForUser();
+		}
+	}
+
 	public static void main(String[] args) {
 		createBoard();
+		chooseCharacterForUser();
 	}
 }
