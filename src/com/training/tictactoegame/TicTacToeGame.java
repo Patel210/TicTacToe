688cfa6b1ecdea1b408add943efd9166b833259d
Patel210 <pranavpatel2101997@gmail.com>
2020-10-05 21:59:57 +0530

    Added method for user to enter and validate the move

--- src/com/training/tictactoegame/TicTacToeGame.java
+++ src/com/training/tictactoegame/TicTacToeGame.java
@@ -22,16 +22,17 @@ public class TicTacToeGame {
 	private static void chooseCharacterForUser() {
 		System.out.println("Enter the Character you want to play for this game (X or O): ");
 		USER = SC.next().charAt(0);
-		if (USER == 'X' || USER == 'O')
+		if (USER == 'X' || USER == 'O') {
 			COMPUTER = (USER == 'X') ? 'O' : 'X';
+		}
 		else {
 			System.out.println("Strictly enter (X or O)");
 			chooseCharacterForUser();
 		}
 	}
-	
+
 	/**
-	 *  To view the board 
+	 * To view the board
 	 */
 	private static void showBoard() {
 		for (int i = 1; i < board.length; i++) {
@@ -43,9 +44,32 @@ public class TicTacToeGame {
 		}
 	}
 
+	/**
+	 * To get the index by user for the move
+	 */
+	private static int getUserMove() {
+		System.out.println("Enter the move (1 -9): ");
+		int index = SC.nextInt();
+		while (true) {
+			if (index >= 1 && index <= 9 && isMovePossible(index)) {
+				return index;
+			}
+		}
+	}
+
+	/**
+	 *  To check whether the move is possible
+	 * 
+	 */
+	private static boolean isMovePossible(int index) {
+
+		return board[index] == ' ';
+	}
+
 	public static void main(String[] args) {
 		createBoard();
 		chooseCharacterForUser();
 		showBoard();
+		getUserMove();
 	}
 }
